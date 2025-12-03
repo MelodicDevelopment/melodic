8cc10fc5a6270b0cc0b4d0bfaba7bc485d181498
Rick Hopkins <rick.hopkins@gmail.com>
2025-12-02 20:40:06 -0500

    Add overloads to signal function for optional initial value
    The signal function now supports overloads for cases with and without an initial value, returning Signal<T> or Signal<T | undefined> respectively. Internal types and subscriber signatures were updated to handle undefined values, improving type safety and flexibility.

--- src/signals/functions/signal.function.ts
+++ src/signals/functions/signal.function.ts
@@ -2,9 +2,16 @@ import { SIGNAL_MARKER, type Signal } from '../types/signal.type';
 import type { Subscriber } from '../types/subscriber.type';
 import { getActiveEffect } from './active-effect.functions';
 
-export const signal = <T>(initialValue: T): Signal<T> => {
+// Overload: when initial value is provided, return Signal<T>
+export function signal<T>(initialValue: T): Signal<T>;
+
+// Overload: when no initial value, return Signal<T | undefined>
+export function signal<T>(): Signal<T | undefined>;
+
+// Implementation signature
+export function signal<T>(initialValue?: T): Signal<T | undefined> {
 	let _value = initialValue;
-	const _subscribers = new Set<Subscriber<T>>();
+	const _subscribers = new Set<Subscriber<T | undefined>>();
 
 	const notify = (): void => {
 		_subscribers.forEach((subscriber) => subscriber(_value));
@@ -13,30 +20,30 @@ export const signal = <T>(initialValue: T): Signal<T> => {
 	const read = (() => {
 		const activeEffect = getActiveEffect();
 		if (activeEffect) {
-			activeEffect.addDependency<T>(read);
+			activeEffect.addDependency<T | undefined>(read);
 			_subscribers.add(activeEffect.execute);
 		}
 
 		return _value;
-	}) as Signal<T>;
+	}) as Signal<T | undefined>;
 
-	read.set = (newValue: T): void => {
+	read.set = (newValue: T | undefined): void => {
 		if (_value !== newValue) {
 			_value = newValue;
 			notify();
 		}
 	};
 
-	read.update = (updater: (current: T) => T): void => {
+	read.update = (updater: (current: T | undefined) => T | undefined): void => {
 		read.set(updater(_value));
 	};
 
-	read.subscribe = (subscriber: Subscriber<T>): (() => void) => {
+	read.subscribe = (subscriber: Subscriber<T | undefined>): (() => void) => {
 		_subscribers.add(subscriber);
 		return () => _subscribers.delete(subscriber);
 	};
 
-	read.unsubscribe = (subscriber: Subscriber<T>): void => {
+	read.unsubscribe = (subscriber: Subscriber<T | undefined>): void => {
 		_subscribers.delete(subscriber);
 	};
 
@@ -51,4 +58,4 @@ export const signal = <T>(initialValue: T): Signal<T> => {
 	});
 
 	return read;
-};
+}
