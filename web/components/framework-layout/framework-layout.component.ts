aee1139223f05ee69c4f1d72a803f8b7ab8d565e
Rick Hopkins <rick.hopkins@gmail.com>
2025-12-28 14:45:19 -0500

    Update framework-layout.component.ts

--- web/components/framework-layout/framework-layout.component.ts
+++ web/components/framework-layout/framework-layout.component.ts
@@ -1,5 +1,4 @@
 import { MelodicComponent } from '../../../src/components';
-import type { IRoute } from '../../../src/routing/interfaces/iroute.interface';
 import { frameworkLayoutTemplate } from './framework-layout.template';
 import { frameworkLayoutStyles } from './framework-layout.styles';
 
